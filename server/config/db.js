31e096991fd3a223752f33ef35d5016d792a1768
Amar <amarkhadkabardiya1234@gmail.com>
2026-04-14 23:41:22 +0545

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,35 @@
+// import mongoose from "mongoose";
+
+// // Function to connect mongoose db 
+// const connectDB = async()=>{
+//     mongoose.connection.on('connected',()=> console.log('Database connected'))
+//     mongoose.connect(`${process.env.MONGO_URI}/job-portal`)
+// }
+
+// export default connectDB;
+
+
 import mongoose from "mongoose";
 
-// Function to connect mongoose db 
-const connectDB = async()=>{
-    mongoose.connection.on('connected',()=> console.log('Database connected'))
-    mongoose.connect(`${process.env.MONGO_URI}/job-portal`)
-}
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) {
+    console.log("⚡ Using existing DB connection");
+    return;
+  }
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "job-portal", 
+    });
+
+    isConnected = conn.connections[0].readyState === 1;
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(" DB Connection Error:", error);
+  }
+};
 
 export default connectDB;
