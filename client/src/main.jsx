53b7c08ef2dfdd1208a8ba8215b5874764925990
Amar <amarkhadkabardiya1234@gmail.com>
2026-04-18 01:05:43 +0545

    check .env file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,20 +7,22 @@ import { AppContextProvider } from './context/AppContext.jsx'
 import {ClerkProvider} from '@clerk/react'
 
 
-//Import publish key
+// Get publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-if(!PUBLISHABLE_KEY){
-  throw new Error("Missing Publishable key")
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}afterSignOutUrl="/" >  
-  <BrowserRouter>
-  <AppContextProvider>
-    <App />
-  </AppContextProvider>
-  </BrowserRouter>
-
-  </ClerkProvider>,
+  <ClerkProvider
+    publishableKey={PUBLISHABLE_KEY}
+    afterSignOutUrl="/"
+  >
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
+  </ClerkProvider>
 )
