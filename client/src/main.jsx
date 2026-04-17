96f2be3d0aea35b30cc8322f321b1b966fc9f670
Amar <amarkhadkabardiya1234@gmail.com>
2026-04-18 01:40:08 +0545

    fixed jwt

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,32 +4,23 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppContextProvider } from './context/AppContext.jsx'
-import { ClerkProvider } from '@clerk/react'
+import {ClerkProvider} from '@clerk/react'
 
 
 //Import publish key
-// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-// if(!PUBLISHABLE_KEY){
-//   throw new Error("Missing Publishable key")
-// }
-
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-console.log("VITE_CLERK_PUBLISHABLE_KEY value:", PUBLISHABLE_KEY)  // Add this
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error(`Missing Publishable key. Got: ${PUBLISHABLE_KEY}`)
+if(!PUBLISHABLE_KEY){
+  throw new Error("Missing Publishable key")
 }
 
-
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" >
-    <BrowserRouter>
-      <AppContextProvider>
-        <App />
-      </AppContextProvider>
-    </BrowserRouter>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}afterSignOutUrl="/" >  
+  <BrowserRouter>
+  <AppContextProvider>
+    <App />
+  </AppContextProvider>
+  </BrowserRouter>
 
   </ClerkProvider>,
 )
